50eae7bbd88277b39b094cede9e62de945836692
ghashy <obsidian.musicwork@gmail.com>
2024-02-05 00:57:13 +0300

    add global state objects with app config and http client into app

--- Acquirust-Client/Acquirust_ClientApp.swift
+++ Acquirust-Client/Acquirust_ClientApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct Acquirust_ClientApp: App {
+    @StateObject private var settings: AppConfig
+    @StateObject private var httpClient: HttpClient
+
+    init() {
+        let config = AppConfig()
+        _settings = StateObject(wrappedValue: config)
+        _httpClient = StateObject(wrappedValue: HttpClient(appConfig: config))
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(settings)
+                .environmentObject(httpClient)
         }
     }
 }
