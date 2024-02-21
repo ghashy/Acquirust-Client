354174f751a8221b09fad7b96d191a8dc92dd1db
ghashy <obsidian.musicwork@gmail.com>
2024-02-21 11:52:31 +0300

    Format code

--- AcquirustClient/AppDelegate.swift
+++ AcquirustClient/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool
+    {
         return true
     }
 }
