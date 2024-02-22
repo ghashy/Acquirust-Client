719fe1e6be1ee90ffaafd9607564160f3aa6c5aa
ghashy <obsidian.musicwork@gmail.com>
2024-02-22 13:50:31 +0300

    Reimplement Tracing websocket connection using URLSessionWebSocketTask

--- AcquirustClient/Model/Tracing.swift
+++ AcquirustClient/Model/Tracing.swift
@@ -6,23 +6,24 @@
 //
 
 import Foundation
-import Starscream
 
 class Tracing: NSObject {
 
-    // Web socket
-    private var socket: WebSocket!
-
+    // Singletone
     static let shared = Tracing.init()
-    static let helper = {
+
+    // State
+    private var socketTask: URLSessionWebSocketTask!
+    private var session: URLSession!
+    private var logs = NSMutableAttributedString()
+    private let helper = {
         let helper = AMR_ANSIEscapeHelper()
         helper.defaultStringColor = .white
         helper.font = NSFont(name: "JetBrains Mono", size: 15)
         return helper
     }
-    
-    var logs = NSMutableAttributedString()
 
+    // Delegate
     weak var delegate: TracingViewController? {
         didSet {
             delegate!.append(with: logs)
@@ -31,7 +32,19 @@ class Tracing: NSObject {
 
     override init() {
         super.init()
-        
+        session = URLSession(
+            configuration: .default, delegate: self,
+            delegateQueue: OperationQueue.main)
+        updateConnection()
+    }
+
+    func updateConnection() {
+        createSocketTask()
+    }
+}
+
+extension Tracing {
+    private func prepareRequest() -> URLRequest {
         let config = AppConfig.shared
         let endpoint = config.data.endpoint
         var request = URLRequest(
@@ -40,41 +53,64 @@ class Tracing: NSObject {
         request.basicAuth(
             username: config.data.username, password: config.data.password)
         request.timeoutInterval = 5
-        socket = WebSocket(request: request)
-        socket.delegate = self
-        socket.connect()
+        return request
     }
     
-    func updateConnection() {
-        socket.forceDisconnect()
-        socket.connect()
+    private func createSocketTask() {
+        socketTask = session.webSocketTask(with: prepareRequest())
+        socketTask.delegate = self
+        socketTask.resume()
     }
-
-}
-
-func convert(input: String) -> NSAttributedString {
-    return Tracing.helper().attributedString(withANSIEscapedString: input)
-}
-
-extension Tracing: WebSocketDelegate {
-    func didReceive(
-        event: Starscream.WebSocketEvent, client: Starscream.WebSocketClient
-    ) {
-        switch event {
-            case .text(let text):
-                let attributed = convert(input: text)
-                logs.append(attributed)
-
-                if let delegate = delegate {
-                    delegate.append(with: convert(input: text))
-                }
-            case .peerClosed:
-                client.disconnect()
-                if let delegate = delegate {
-                    delegate.append(with: convert(input: "Connection lost!"))
-                }
-            default:
-                return
+    
+    private func convert(input: String) -> NSAttributedString {
+        return helper().attributedString(withANSIEscapedString: input)
+    }
+    
+    private func update(input: String) {
+        let attributed = convert(input: input)
+        logs.append(attributed)
+        if let delegate = delegate {
+            delegate.append(with: attributed)
         }
     }
+    
+    private func receive() {
+        socketTask?.receive(completionHandler: { [weak self] result in
+            switch result {
+                case .success(let message):
+                    switch message {
+                        case .string(let logs):
+                            self?.update(input: logs)
+                        default:
+                            break
+                    }
+                case .failure(let error):
+                    print("Failed to receive message over ws: \(error)")
+                    self?.update(input: "\nDisconnected from server!\n\n")
+                    return
+            }
+            self?.receive()
+        })
+    }
 }
+
+// MARK: Notifier: URLSessionWebSocketDelegate
+extension Tracing: URLSessionWebSocketDelegate {
+    func urlSession(
+        _ session: URLSession, webSocketTask: URLSessionWebSocketTask,
+        didOpenWithProtocol p: String?
+    ) {
+        print("Did open url socket session with protocol: \(String(describing: p))")
+        update(input: "Connected to server!\n\n")
+        receive()
+    }
+    func urlSession(
+        _ session: URLSession, webSocketTask: URLSessionWebSocketTask,
+        didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?
+    ) {
+        print("Did close url socket session with reason: \(String(describing: reason))")
+    }
+}
+
+// MARK: URLSessionDelegate
+extension Tracing: URLSessionDelegate {}
